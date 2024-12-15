cbbc6758b45c6a1f6c02fc1f6be60865762fc373
Derek Zhu <derekzhu@Dereks-MacBook-Pro.local>
2024-12-14 19:52:50 -0800

    Added stuff to homepage

--- app/page.js
+++ app/page.js
@@ -4,13 +4,29 @@ import classes from './page.module.css'
 export default function Home() {
   return (
     <>
-    <header>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
 
-    </header>
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Observing & Documenting the Paranormal</h1>
+            <p>Unveiling Unknowns Together</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/watch">Explore Current Watches</Link>
+          </div>
+        </div>
+      </header>
       <main>
-        <p><Link href="/watch">Watches</Link></p>
-        <p><Link href="/watch/share">Share Watch</Link></p>
-        <p><Link href="/watch/share">Community</Link></p>
+        <section className={classes.section}>
+          <h2>How our forum works</h2>
+          <p>
+            Parawatch is more than just an internet forum. With the development of webscrapers and interesting dataflows
+            we scrape the internet looking for activities of interest that may or may not be the result of paranormal incidents.
+          </p>
+        </section>
       </main>
     </>
   );
